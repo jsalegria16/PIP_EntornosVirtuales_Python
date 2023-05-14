215798fed67c8c920436eb8751e47994a468a769
JhonAlegria_Git <alegriasebas.sa@hotmail.com>
2023-05-14 10:08:52 -0500

    Docker to AppFinal34

--- AppFinal34_GlobalPopulation/grafico.py
+++ AppFinal34_GlobalPopulation/grafico.py
@@ -14,7 +14,7 @@ def generatePieChart(Labels,values):
     axes.pie(values,labels=Labels)
     axes.axis('equal')
     plot.xticks(rotation=90)
-    plot.savefig(f'./imgs/{namePie}IMG.png')
+    plot.savefig(f'./imgs/{namePie}IMGGGGGG.png')
     
     
 if __name__ == '__main__':
